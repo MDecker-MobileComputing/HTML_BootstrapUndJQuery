534c39ecd782575b2842320f14cb8b01ad531f65
MDecker-MobileComputer <mide_42b@yahoo.de>
2019-12-09 19:59:53 +0100

    Passwortgenerator: Kleine Code-Formatierungs-Verbesserungen.

--- docs/PasswortGenerator/meinCode.js
+++ docs/PasswortGenerator/meinCode.js
@@ -4,26 +4,26 @@
  * Funktion zum eigentlichen Erzeugen des zufälligen Passworts.
  * Das erzeugte Passwort wird als String zurückgegeben.
  * Diese Funktion enthält keinen jQuery-spezifischen Code.
- * 
+ *
  * @param zeichenVorrat  String mit den Zeichen, die für das Passwort zur Verfügung stehen,
  *                       z.B. "abcde...0123456789" -- jedes Zeichen darf nur einmal auftauchen!
- * 
+ *
  * @param laenge  Anzahl Zeichen, die das erzeugte Passwort haben soll.
  */
 function passwortErzeugen(zeichenVorrat, laenge) { "use strict";
-    
+
     const anzZeichen = zeichenVorrat.length;
-    
+
     let passwortString = "";
     for (let i = 1; i <= laenge; i++) {
-        
+
         // Zufälliges Zeichen aus dem String "zeichenVorrat" auswählen
         let zufallsIndex   = Math.floor(Math.random() * anzZeichen);
         let zufallsZeichen = zeichenVorrat.charAt(zufallsIndex);
-        
+
         passwortString += zufallsZeichen;
     }
-   
+
     return passwortString;
 }
 
@@ -35,13 +35,13 @@ function passwortErzeugenEventHandler() { "use strict";
 
     var zeichenVorrat = "";
     if ( $("#buchstaben").prop("checked") === true ) {
-        
+
         zeichenVorrat = "ABCDEFGHJKLMNPQRSTVXYZ";
-        
+
     } else if ( $("#buchstabenZiffern").prop("checked") === true ) {
 
-        zeichenVorrat = "ABCDEFGHJKLMNPQRSTVXYZ123456789"; // Keine "0" (Null), wegen Verwechselungsgefahr mit Buchstabe "O"        
-        
+        zeichenVorrat = "ABCDEFGHJKLMNPQRSTVXYZ123456789"; // Keine "0" (Null) wegen Verwechselungsgefahr mit Buchstabe "O"
+
     } else {
 
         zeichenVorrat = "ABCDEFGHJKLMNPQRSTVXYZ123456789+-*/=?!.,=:§$%&()[]";
@@ -65,4 +65,4 @@ function onSeiteGeladen() { "use strict";
 }
 
 
-$(document).ready( onSeiteGeladen ); 
+$(document).ready( onSeiteGeladen );
